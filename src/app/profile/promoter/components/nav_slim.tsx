20d851a55cf409243e884d8d6f062303678aa3be
Washington Oliveira <wdeoliveiraj@gmail.com>
2023-05-05 22:26:56 -0300

    ...

--- src/app/profile/promoter/components/nav_slim.tsx
+++ src/app/profile/promoter/components/nav_slim.tsx
@@ -15,7 +15,7 @@ function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function Nav_Tailwind() {
+export default function Nav_Tailwind_Slim() {
   return (
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
@@ -33,45 +33,17 @@ export default function Nav_Tailwind() {
                   )}
                 </Disclosure.Button>
               </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+              <div className="flex flex-1 items-center justify-center sm:items-stretch">
                 <div className="flex flex-shrink-0 items-center">
-                  <img
-                    className="block h-8 w-auto lg:hidden"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company"
-                  />
                   <img
                     className="hidden h-8 w-auto lg:block"
                     src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
                     alt="Your Company"
                   />
                 </div>
-                <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex space-x-4">
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'rounded-md px-3 py-2 text-sm font-medium'
-                        )}
-                        aria-current={item.current ? 'page' : undefined}
-                      >
-                        {item.name}
-                      </a>
-                    ))}
-                  </div>
-                </div>
               </div>
+
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                >
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
 
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
@@ -85,6 +57,7 @@ export default function Nav_Tailwind() {
                       />
                     </Menu.Button>
                   </div>
+
                   <Transition
                     as={Fragment}
                     enter="transition ease-out duration-100"
@@ -94,6 +67,8 @@ export default function Nav_Tailwind() {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                   >
+
+                    {/* Itens para o dropdown no icone do avatar */}
                     <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <Menu.Item>
                         {({ active }) => (
@@ -133,24 +108,6 @@ export default function Nav_Tailwind() {
             </div>
           </div>
 
-          <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 px-2 pb-3 pt-2">
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'block rounded-md px-3 py-2 text-base font-medium'
-                  )}
-                  aria-current={item.current ? 'page' : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
-            </div>
-          </Disclosure.Panel>
         </>
       )}
     </Disclosure>
--- src/app/profile/promoter/page.tsx
+++ src/app/profile/promoter/page.tsx
@@ -1,9 +1,13 @@
-import Nav_Tailwind from "./components/nav";
+import Nav_Tailwind_Complete from "./components/nav_complete";
+import Nav_Tailwind_Slim from "./components/nav_slim";
+import Section_Form_TailWind from "./components/section";
 
 export default function Home() {
     return (<div>
-      <Nav_Tailwind/>
-      <p>Tela perfil de promoter</p>
+      <Nav_Tailwind_Slim/>
+      <div className="w80 container">
+      <Section_Form_TailWind/>
+      </div>
     </div>
     )
   }
